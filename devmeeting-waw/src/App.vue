a3aaa1c6f65ffb4838c8d318729a8be4f8cddc63
Vik Sok <skinyman@meta.ua>
2018-09-15 13:25:42 +0200

    [#2.1] [#2.2] [#2.3]

--- devmeeting-waw/src/App.vue
+++ devmeeting-waw/src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Smartphones list</h2>
+    <ul>
+      <li v-for="(s, index) in smartphones">{{ s }} {{ index }}
+        <button @click="removeElem(index)" key>Remove</button>
+      </li>
+    </ul>
+
+    <p v-if="!smartphones.length">No products!</p>
+
+    <form @submit.prevent="addElem()">
+      <input
+          type="text"
+          name="newVendor"
+          v-model="newElem"
+          v-validate="'required'"
+      >
+      <button>Add vendor</button>
+
+      <div v-show="errors.has('newVendor')">
+        {{ errors.first('newVendor') }}
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      smartphones: ['Moto', 'Huawei'],
+      newElem: '' //init
+    }
+  },
+  methods: {
+    addElem() {
+      this.$validator.validateAll()
+        .then(result => {
+          if (!result) {
+            return;
+          }
+          this.smartphones.push(this.newElem);
+          this.newElem = ''; // clear input value on button pressed
+          this.$validator.reset(); // reset validator state
+        })
+    },
+    removeElem(index) {
+      this.smartphones.splice(index, 1);
+    }
   }
 }
 </script>
